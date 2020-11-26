6e0b5f23b6738e711372cfd05823382f9dd0ecb8
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-11-26 20:54:09 +0100

    First commit

---
+++ src/test/java/com/crud/bets/BetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.bets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
